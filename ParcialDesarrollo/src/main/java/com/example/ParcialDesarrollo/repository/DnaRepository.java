f44d65132eeff2dd860d957b5e7db75c3e66168b
bianpalazzo <bianpalazzo@hotmail.com>
2024-11-02 23:44:03 +0000

    Delete ParcialDesarrollo directory

--- ParcialDesarrollo/src/main/java/com/example/ParcialDesarrollo/repository/DnaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ParcialDesarrollo.repository;
-
-import com.example.ParcialDesarrollo.model.DnaRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface DnaRepository extends JpaRepository<DnaRecord, Long> {
-    Optional<DnaRecord> findByDnaSequence(String dnaSequence);
-}
