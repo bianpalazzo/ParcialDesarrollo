9a91050bc992b6bdef5fb5e96d79ebcb6e8d4bb2
bianpalazzo <bianpalazzo@hotmail.com>
2024-11-02 22:43:45 +0000

    Add files via upload

---
+++ ParcialDesarrollo/src/main/java/com/example/ParcialDesarrollo/ParcialDesarrolloApplication.java
@@ -0,0 +1,12 @@
+package com.example.ParcialDesarrollo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialDesarrolloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialDesarrolloApplication.class, args);
+	}
+}
