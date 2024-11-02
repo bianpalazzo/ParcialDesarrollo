8fca2aefbaf1df6e73e5c9ae65194975eb9d3447
bianpalazzo <bianpalazzo@hotmail.com>
2024-11-02 23:45:23 +0000

    Add files via upload

---
+++ ParcialDesarrollo/src/test/java/com/example/ParcialDesarrollo/ParcialDesarrolloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ParcialDesarrollo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialDesarrolloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
